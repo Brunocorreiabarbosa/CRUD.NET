a169f23aad4ea7a5f30f48c7b3265108f435fefa
Brunocorreiabarbosa <brunocorreiabarbosa@gmail.com>
2022-09-27 18:44:15 -0300

    Primeiro Commit

---
+++ CRUD/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+    }
+}
